9e4b51ac591bfb602a4946ba5f36b63d792e07bf
codeYann <ccyan05@alu.ufc.br>
2023-04-06 21:46:31 -0300

    Refactoring AFN

--- api/afn.py
+++ api/afn.py
@@ -1,21 +1,27 @@
 from typing import List, Dict, Set
+from collections import defaultdict
 
-State = int
+State = int | str
+Symbol = str
 
 
 class AFN:
     def __init__(self, list_states: List[State]) -> None:
         self.list_states: List[State] = list_states
         self.final_states: Set[State] = set()
-        self.transitions: Dict[State, Dict[str, Set[State]]] = {}
+        self.transitions: Dict[State, Dict[Symbol, Set[State]]] = defaultdict(
+            lambda: defaultdict(set)
+        )
         self.initial_state: State = list_states[0]
 
-    def add_transition(self, src_state: State, symbol: str, dst_state: State) -> None:
+    def add_transition(
+        self, src_state: State, symbol: Symbol, dst_state: State
+    ) -> None:
         if src_state not in self.list_states:
-            raise Exception(f"{src_state} state is not in set of state")
+            raise ValueError(f"{src_state} state is not in set of state")
 
         if dst_state not in self.list_states:
-            raise Exception(f"{dst_state} state is not in set of state")
+            raise ValueError(f"{dst_state} state is not in set of state")
 
         if src_state not in self.transitions:
             self.transitions[src_state] = {}
@@ -25,7 +31,7 @@ class AFN:
     def set_final_states(self, states: List[State]) -> None:
         self.final_states = set(states)
 
-    def get_states(self, src_state: State, symbol: str) -> Set[State]:
+    def get_states(self, src_state: State, symbol: Symbol) -> Set[State]:
         if src_state not in self.transitions:
             raise Exception(f"This {src_state} was not previusly added.")
 
@@ -34,27 +40,41 @@ class AFN:
 
         return self.transitions[src_state][symbol]
 
-    def accepts(self, chain: str) -> bool:
-        state_pointer = self.initial_state
+    def accepts(self, chain: List[Symbol]) -> bool:
+        pointer = self.initial_state
 
         for i in range(len(chain)):
-            current_state = self.get_states(state_pointer, chain[i])
-            state_pointer = current_state.copy().pop()
+            current_state = self.get_states(pointer, chain[i])
+            pointer = current_state.copy().pop()
 
-        if state_pointer in self.final_states:
+        if pointer in self.final_states:
             return True
 
         return False
 
-    def counting_patterns(self, chain: str) -> int:
-        state_pointer = self.initial_state
-        counter = 0
-        for i in range(len(chain)):
-            current_state = self.get_states(state_pointer, chain[i])
-            state_pointer = current_state.copy().pop()
+    def counting_patterns(self, chain: List[Symbol]) -> int:
+        count = 0
+        i = 0
+        while i < len(chain):
+            pointer = self.initial_state
+            last_symbol = None
+            while i < len(chain):
+                current_state = self.get_states(pointer, chain[i])
+                if current_state:
+                    last_symbol = chain[i]
+                    pointer = current_state.copy().pop()
+                    i += 1
+                else:
+                    break
+            if pointer in self.final_states and last_symbol is not None:
+                count += 1
+                # Recover last symbol and move back to initial state
+                current_state = self.get_states(pointer, last_symbol)
+                pointer = current_state.copy().pop()
+            else:
+                # Move back to initial state without recovering last symbol
+                pointer = self.initial_state
+            if pointer not in self.transitions:
+                break
 
-            if state_pointer in self.final_states:
-                counter += 1
-                state_pointer = self.initial_state
-
-        return counter
+        return count
