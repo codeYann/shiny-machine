f25fd6ba8cdc2cf41116da0f3ed90fdae6c53c08
codeYann <ccyan05@alu.ufc.br>
2023-04-14 17:51:54 -0300

    Improving NFA code

--- api/afn.py
+++ api/afn.py
@@ -1,4 +1,5 @@
-from typing import List, Dict, Set, Tuple
+from re import S
+from typing import List, Dict, Set
 from collections import defaultdict
 
 State = int | str
@@ -13,6 +14,7 @@ class AFN:
             lambda: defaultdict(set)
         )
         self.initial_state: State = list_states[0]
+        self.patterns: Set[State] = set()
 
     def add_transition(
         self, src_state: State, symbol: Symbol, dst_state: State
@@ -31,6 +33,9 @@ class AFN:
     def set_final_states(self, states: List[State]) -> None:
         self.final_states = set(states)
 
+    def set_pattenrs(self, patterns: List[State]) -> None:
+        self.patterns = set(patterns)
+        
     def get_states(self, src_state: State, symbol: Symbol) -> Set[State]:
         if src_state not in self.transitions:
             raise Exception(f"This {src_state} was not previusly added.")
@@ -57,11 +62,29 @@ class AFN:
         pointer = self.initial_state
         current_state: Set[State] | None = None
         for j in range(len(chain)):
-            if pointer in self.final_states:
-                count += 1
-                current_state = self.get_states(pointer, chain[j - 1])
-                pointer = current_state.copy().pop()
-
             current_state = self.get_states(pointer, chain[j])
             pointer = current_state.copy().pop()
+            if pointer in self.final_states:
+                count += 1
+                pointer = self.initial_state
         return count
+
+
+    def recognize_patterns(self, chain: List[Symbol], other_case: List[Symbol]) -> Dict[State, int]
+        pointer = self.initial_state
+        counter: Dict[State, int] = {}
+
+        for i in self.patterns:
+            counter[i] = 0
+
+        for j in range(len(chain)):
+            symbol = chain[j]
+            if pointer in self.final_states and symbol in other_case:
+                current_state = self.get_states(pointer, symbol)
+                new_pointer = current_state.copy().pop()
+                counter[new_pointer] = int(counter[new_pointer]) + 1
+
+            current_state = self.get_states(pointer, symbol)
+            pointer = current_state.copy().pop()
+
+        return counter
