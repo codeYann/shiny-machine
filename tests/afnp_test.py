3350f232c309a108de2e9a1d3eb53dee7a3c6b62
codeYann <ccyan05@alu.ufc.br>
2023-04-26 19:19:54 -0300

    Testing afnp

--- tests/afnp_test.py
+++ tests/afnp_test.py
@@ -1,11 +1,35 @@
-# import pytest
-from string import digits
+import string
 from api.afnp import AFNP
 
 
 class TestAFNP:
-    def test_get_states(self) -> None:
-        afnp = AFNP(["q0", "q1", "q2"])
-        afnp.add_transition("q0", digits, "q0")
+    """Creating a bunch of unit test in AFNP Class"""
 
-        assert afnp.get_states("q0", "1") == set("q0")
+    def test_afnp_attributes(self) -> None:
+        """
+        Testing if there's any substr of string.digits and . in afnp.else_case
+        """
+        afnp = AFNP(["q0", "q1", "q2"])
+        invalid_chars = [c for c in "".join([string.digits, "."])]
+        assert any(c in afnp.else_case for c in invalid_chars) is False
+
+    def test_afnp_get_states(self) -> None:
+        """
+        AFNP's get_states method has a several differences beetwen AFN class.
+
+        Here do we need to check if the symbol is in string.digits
+        and if digits are in src_state keys list.
+
+        We also check if the symbol is in afnp.else_case
+        and if else_case are in src_state keys list.
+
+        If any of this conditional are sastisfied then we return
+        self.transition[src_state][digits] or
+        self.transition[src_state][afnp.else_case]
+        """
+
+        afnp = AFNP(["q0", "q1", "q2"])
+        afnp.add_transition("q0", afnp.else_case, "q1")
+        response = afnp.get_states("q0", "$")
+        print(afnp.transitions)
+        assert response == {"q1"}
